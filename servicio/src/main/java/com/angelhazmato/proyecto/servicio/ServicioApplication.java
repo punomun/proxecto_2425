53acacd8d35e4d438ff99280328b7f2ebc6199a2
Ángel Haz Mato <angelhazmato@gmail.com>
2025-05-07 10:05:27 +0200

    Añadir servicio con application.properties

---
+++ servicio/src/main/java/com/angelhazmato/proyecto/servicio/ServicioApplication.java
@@ -0,0 +1,12 @@
+package com.angelhazmato.proyecto.servicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioApplication.class, args);
+    }
+}
