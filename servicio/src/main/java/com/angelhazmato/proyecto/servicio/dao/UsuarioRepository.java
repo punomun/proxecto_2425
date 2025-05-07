0a89e3ef7c54b13ec240538b192ca55996b86e2e
Ángel Haz Mato <angelhazmato@gmail.com>
2025-05-07 13:25:30 +0200

    Expandir UsuarioRepository

--- servicio/src/main/java/com/angelhazmato/proyecto/servicio/dao/UsuarioRepository.java
+++ servicio/src/main/java/com/angelhazmato/proyecto/servicio/dao/UsuarioRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     void deleteByNombre(String nombre);
+
+    boolean existsByNombre(String nombre);
 }
