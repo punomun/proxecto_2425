5a011401b0b5ba8105526ef64f3258c4371a8ae2
Ángel Haz Mato <angelhazmato@gmail.com>
2025-05-15 19:36:53 +0200

    Mover lógica de EventoVista a EventosControlador y añadir lugar a la vista

--- app/app/src/main/java/com/example/vibragenda/vista/EventoVista.kt
+++ app/app/src/main/java/com/example/vibragenda/vista/EventoVista.kt
@@ -1,9 +1,6 @@
 package com.example.vibragenda.vista
 
-import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -11,18 +8,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vibragenda.R
-import com.example.vibragenda.modelo.ServicioApi
-import com.example.vibragenda.vista.adaptador.ArtistaAdaptador
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.util.Base64
+import com.example.vibragenda.controlador.EventosControlador
 
 class EventoVista : AppCompatActivity() {
+    lateinit var img: ImageView
+    lateinit var nombre: TextView
+    lateinit var fecha: TextView
+    lateinit var lugar: TextView
+    lateinit var artistasConfirmados: TextView
+    lateinit var spinner: ProgressBar
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,47 +30,18 @@ class EventoVista : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        img = findViewById(R.id.imgVwEvento)
+        nombre = findViewById(R.id.txtVwEventoNombre)
+        fecha = findViewById(R.id.txtVwEventoFecha)
+        lugar = findViewById(R.id.txtVwEventoLugar)
+        artistasConfirmados = findViewById(R.id.txtVwEventoArtistas)
+        spinner = findViewById(R.id.progressBar)
+        recyclerView = findViewById(R.id.rvEventoArtistas)
 
-        val img = findViewById<ImageView>(R.id.imgVwEvento)
-        val nombre = findViewById<TextView>(R.id.txtVwEventoNombre)
-        val fecha = findViewById<TextView>(R.id.txtVwEventoFecha)
-        val artistasConfirmados = findViewById<TextView>(R.id.txtVwEventoArtistas)
-        val spinner = findViewById<ProgressBar>(R.id.progressBar)
-        val recyclerView = findViewById<RecyclerView>(R.id.rvEventoArtistas)
-
-        VistaUtils.ocultarDatos(spinner, img, nombre, fecha, artistasConfirmados, recyclerView)
-
+        VistaUtils.ocultarDatos(spinner, img, nombre, fecha, lugar, artistasConfirmados, recyclerView)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        lifecycleScope.launch(Dispatchers.IO) {
-            val evento = ServicioApi.obtenerEvento(intent.getIntExtra("EVENTO_ID", 1))
-            val artistasEvento = ServicioApi.obtenerArtistasDeEvento(evento.id)
-            Log.i("AHM", "Num artistas: ${artistasEvento.size}, evento id: ${evento.id}")
-            withContext(Dispatchers.Main) {
-                try {
-                    val byteArray = Base64.getDecoder().decode(evento.imagen)
-                    val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-                    img.setImageBitmap(bitmap)
-                } catch (e: Exception) {
-                    img.setImageResource(R.drawable.no_image_18x10)
-                }
-                nombre.text = evento.nombre
-                fecha.text = evento.fecha
-
-                if (artistasEvento.isNotEmpty()) {
-                    val artistaAdaptador = ArtistaAdaptador(artistasEvento) { artista  ->
-                        val intent = Intent(this@EventoVista, ArtistaVista::class.java)
-                        intent.putExtra("ARTISTA_ID", artista.id)
-                        startActivity(intent)
-                    }
-                    recyclerView.adapter = artistaAdaptador
-                    VistaUtils.mostrarDatos(spinner, img, nombre, fecha, artistasConfirmados, recyclerView)
-                } else {
-                    artistasConfirmados.text = getString(R.string.no_hay_artistas_confirmados)
-                    VistaUtils.mostrarDatos(spinner, img, nombre, fecha, artistasConfirmados)
-                }
-            }
-        }
+        EventosControlador.cargarEvento(this)
     }
 }
