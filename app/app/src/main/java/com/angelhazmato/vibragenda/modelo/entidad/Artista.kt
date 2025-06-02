d82a9244834e0781bb004a980f928289bf38d1ce
Ángel Haz Mato <angelhazmato@gmail.com>
2025-06-02 16:47:42 +0200

    Añadir nulabilidad a entidades Android

--- app/app/src/main/java/com/angelhazmato/vibragenda/modelo/entidad/Artista.kt
+++ app/app/src/main/java/com/angelhazmato/vibragenda/modelo/entidad/Artista.kt
@@ -3,8 +3,8 @@ package com.angelhazmato.vibragenda.modelo.entidad
 data class Artista(
     var id: Int = 0,
     var nombre: String = "",
-    var descripcion: String = "",
+    var descripcion: String? = "",
     var fechaFormacion: String = "",
-    var imagen: String = "",
-    var icono: String = ""
+    var imagen: String? = "",
+    var icono: String? = ""
 )
--- app/app/src/main/java/com/angelhazmato/vibragenda/modelo/entidad/Evento.kt
+++ app/app/src/main/java/com/angelhazmato/vibragenda/modelo/entidad/Evento.kt
@@ -3,8 +3,8 @@ package com.angelhazmato.vibragenda.modelo.entidad
 data class Evento(
     var id: Int = 0,
     var nombre: String = "",
-    var fecha: String = "",
-    var lugar: String = "",
-    var imagen: String = "",
-    var icono: String = ""
+    var fecha: String? = "",
+    var lugar: String? = "",
+    var imagen: String? = "",
+    var icono: String? = ""
 )
