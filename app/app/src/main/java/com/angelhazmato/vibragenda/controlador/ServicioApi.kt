7396d62db19c46b68fce9f03ef113f111971783e
punomun <angelhazmato@gmail.com>
2025-06-04 19:32:34 +0200

    Ampliar casos de uso de conexión REST

--- app/app/src/main/java/com/angelhazmato/vibragenda/controlador/ServicioApi.kt
+++ app/app/src/main/java/com/angelhazmato/vibragenda/controlador/ServicioApi.kt
@@ -4,13 +4,18 @@ import com.angelhazmato.vibragenda.modelo.entidad.Artista
 import com.angelhazmato.vibragenda.modelo.entidad.Evento
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import java.io.IOException
 
 object ServicioApi {
     lateinit var url: String
+
+    /* ARTISTA */
+
     fun obtenerArtistas(): ArrayList<Artista> {
         var lista = ArrayList<Artista>()
         val client = OkHttpClient()
@@ -18,23 +23,9 @@ object ServicioApi {
         client.newCall(request).execute().use { response: Response ->
             if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-            val responseData = response.body()!!.string()
+            val responseData = response.body!!.string()
             val gson = Gson()
             lista = gson.fromJson(responseData, object : TypeToken<List<Artista>>() {}.type)
-        }
-        return lista
-    }
-
-    fun obtenerEventos(): ArrayList<Evento> {
-        var lista = ArrayList<Evento>()
-        val client = OkHttpClient()
-        val request = Request.Builder().url("$url/api/eventos").build()
-        client.newCall(request).execute().use { response: Response ->
-            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-            val responseData = response.body()!!.string()
-            val gson = Gson()
-            lista = gson.fromJson(responseData, object : TypeToken<List<Evento>>() {}.type)
         }
         return lista
     }
@@ -46,25 +37,11 @@ object ServicioApi {
         client.newCall(request).execute().use { response: Response ->
             if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-            val responseData = response.body()!!.string()
+            val responseData = response.body!!.string()
             val gson = Gson()
             artista = gson.fromJson(responseData, object : TypeToken<Artista>() {}.type)
         }
         return artista
-    }
-
-    fun obtenerEvento(id: Int): Evento {
-        var evento = Evento()
-        val client = OkHttpClient()
-        val request = Request.Builder().url("$url/api/eventos/$id").build()
-        client.newCall(request).execute().use { response: Response ->
-            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-            val responseData = response.body()!!.string()
-            val gson = Gson()
-            evento = gson.fromJson(responseData, object : TypeToken<Evento>() {}.type)
-        }
-        return evento
     }
 
     fun obtenerArtistasDeEvento(idEvento: Int): List<Artista> {
@@ -74,11 +51,99 @@ object ServicioApi {
         client.newCall(request).execute().use { response: Response ->
             if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-            val responseData = response.body()!!.string()
+            val responseData = response.body!!.string()
             val gson = Gson()
             lista = gson.fromJson(responseData, object : TypeToken<List<Artista>>() {}.type)
         }
         return lista
+    }
+
+    fun crearArtista(artista: Artista): Boolean {
+        var artistaCreado: Artista? = null
+        val client = OkHttpClient()
+        val gson = Gson()
+        val body = gson.toJson(artista).toRequestBody("application/json; charset=utf-8".toMediaType())
+
+        val request = Request.Builder()
+            .url("$url/api/artistas")
+            .post(body)
+            .build()
+
+        client.newCall(request).execute().use {response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            val responseData = response.body!!.string()
+            artistaCreado = gson.fromJson(responseData, object : TypeToken<Artista>() {}.type)
+            if (artistaCreado != null) return true
+        }
+        return false
+    }
+
+    fun eliminarArtista(id: Int): Boolean {
+        val client = OkHttpClient()
+        val request = Request.Builder().url("$url/api/artistas/$id").delete().build()
+        client.newCall(request).execute().use { response ->
+            return response.isSuccessful
+        }
+    }
+
+    /* EVENTO */
+
+    fun obtenerEventos(): ArrayList<Evento> {
+        var lista = ArrayList<Evento>()
+        val client = OkHttpClient()
+        val request = Request.Builder().url("$url/api/eventos").build()
+        client.newCall(request).execute().use { response: Response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            val responseData = response.body!!.string()
+            val gson = Gson()
+            lista = gson.fromJson(responseData, object : TypeToken<List<Evento>>() {}.type)
+        }
+        return lista
+    }
+
+    fun obtenerEvento(id: Int): Evento {
+        var evento = Evento()
+        val client = OkHttpClient()
+        val request = Request.Builder().url("$url/api/eventos/$id").build()
+        client.newCall(request).execute().use { response: Response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            val responseData = response.body!!.string()
+            val gson = Gson()
+            evento = gson.fromJson(responseData, object : TypeToken<Evento>() {}.type)
+        }
+        return evento
+    }
+
+    fun crearEvento(evento: Evento): Boolean {
+        var eventoCreado: Evento? = null
+        val client = OkHttpClient()
+        val gson = Gson()
+        val body = gson.toJson(evento).toRequestBody("application/json; charset=utf-8".toMediaType())
+
+        val request = Request.Builder()
+            .url("$url/api/eventos")
+            .post(body)
+            .build()
+
+        client.newCall(request).execute().use {response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            val responseData = response.body!!.string()
+            eventoCreado = gson.fromJson(responseData, object : TypeToken<Evento>() {}.type)
+            if (eventoCreado != null) return true
+        }
+        return false
+    }
+
+    fun eliminarEvento(id: Int): Boolean {
+        val client = OkHttpClient()
+        val request = Request.Builder().url("$url/api/eventos/$id").delete().build()
+        client.newCall(request).execute().use { response ->
+            return response.isSuccessful
+        }
     }
 
     fun obtenerEventosDeArtista(idArtista: Int): List<Evento> {
@@ -88,7 +153,7 @@ object ServicioApi {
         client.newCall(request).execute().use { response: Response ->
             if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-            val responseData = response.body()!!.string()
+            val responseData = response.body!!.string()
             val gson = Gson()
             lista = gson.fromJson(responseData, object : TypeToken<List<Evento>>() {}.type)
         }
