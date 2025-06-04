8cf08bcd767542ad51f26058d438c8901848d890
punomun <angelhazmato@gmail.com>
2025-06-04 19:31:59 +0200

    Arreglar controlador de excepciones

--- app/app/src/main/java/com/angelhazmato/vibragenda/controlador/ControladorExcepciones.kt
+++ app/app/src/main/java/com/angelhazmato/vibragenda/controlador/ControladorExcepciones.kt
@@ -3,13 +3,16 @@ package com.angelhazmato.vibragenda.controlador
 import android.content.Context
 import android.content.Intent
 import com.angelhazmato.vibragenda.vista.ExcepcionActivity
+import java.io.IOException
 
 class ControladorExcepciones(private val applicationContext: Context) :
     Thread.UncaughtExceptionHandler {
 
     override fun uncaughtException(thread: Thread, throwable: Throwable) {
-        val intent = Intent(applicationContext, ExcepcionActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        applicationContext.startActivity(intent)
+        if (throwable is IOException) {
+            val intent = Intent(applicationContext, ExcepcionActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            applicationContext.startActivity(intent)
+        }
     }
 }
